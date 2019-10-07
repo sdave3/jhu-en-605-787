8acddef505c1407ee02d84910786cbd71339dc52
Dave <sdave@C02XF1FLJHD2.local>
2019-10-07 03:10:41 -0400

    module 6 solution

--- module6-solution/js/app.js
+++ module6-solution/js/app.js
@@ -19,15 +19,21 @@
         $scope.tooMuchMsgColorStyle = "border:3px solid";
         $scope.tooMuchMsg = "Please enter data first";
       }else {
-        var lunchMenuArr = $scope.lunchMenu.split(',');
-        lunchMenuArr = lunchMenuArr.filter(Boolean);
         $scope.tooMuchMsgColor = "green";
         $scope.tooMuchMsgColorStyle = "border:3px solid";
+        var lunchMenuArr = $scope.lunchMenu.split(',');
+        lunchMenuArr = lunchMenuArr.filter(Boolean);
+        for ( var x in lunchMenuArr){
+          if ((lunchMenuArr[x].trim()).length === 0 ){
+             lunchMenuArr.splice(x,1);
+          }
+        }
         if(lunchMenuArr.length <= 3) {
           $scope.tooMuchMsg = "Enjoy!";
         }else if(lunchMenuArr.length > 3){
           $scope.tooMuchMsg = "Too much!";
         }
+        $scope.testmsg = lunchMenuArr;
       }
     };
   };
