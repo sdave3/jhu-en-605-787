714eeb7c10754ae63e6e2aecdcd4b3716af3608a
Dave <sdave@C02XF1FLJHD2.local>
2019-11-05 23:59:11 -0500

    module 8 solution

---
+++ module8-solution/js/app.js
@@ -0,0 +1,73 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+	.controller('NarrowItDownController', NarrowItDownController)
+	.service ('MenuSearchService', MenuSearchService)
+	.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com/menu_items.json")
+	.directive('foundItems', FoundItemsDirective);
+
+// Controller
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+	var menu = this;
+	menu.searchTerm = "";
+	menu.found = [];
+
+	// Matches for the term
+	menu.searchItem = function () {
+		var promise = MenuSearchService.getMatchedMenuItems(menu.searchTerm );
+		promise.then(
+			function (response) {
+				menu.found = response.data;
+			}
+		).catch(
+			function (error) {
+				console.log("Something went terribly wrong.");
+			}
+		);
+	};
+
+	// removes from the list of found terms
+	menu.removeItem = function (index) {
+		menu.found.splice(index, 1);
+	}
+}
+
+//Service
+MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+function MenuSearchService($http, ApiBasePath) {
+	var service = this;
+	service.getMatchedMenuItems = function (searchTerm){
+		return $http({
+			method: "GET",
+			url: (ApiBasePath)
+		}).then( function (response) {
+			 var foundItems = response.data.menu_items.filter(matchTerm => matchTerm.description.includes(searchTerm));
+			 console.log(foundItems);
+			 return foundItems;
+		});
+	};
+}
+
+// Directives
+function FoundItemsDirective() {
+	var ddo = {
+		templateUrl: 'foundItems.html',
+		restrict: "E",
+		scope: {
+			foundItems: '<',
+			onRemove: '&'
+		},
+		controller: FoundItemsController,
+		controllerAs: 'menu',
+		bindToController: true
+	};
+  return ddo;
+}
+
+// Directive -- Controller
+function FoundItemsController(){
+	var menu = this;
+}
+})();
